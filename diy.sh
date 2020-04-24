d6e12a2757f8ee34af0aee9c8b4fa3d79c9e291a
yylemon9527 <32920982+yylemon9527@users.noreply.github.com>
2020-04-24 23:25:58 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,5 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+wget https://github.com/melsem/openwrt-lede_xradio-xr819_soc-audio/blob/master/add-SOC_AUDIO-usb2-usb3-opi_zero_openwrt-master-4.19/add-on_wifi-mac80211.patch
+patch -p1 < add-on_wifi-mac80211.patch
