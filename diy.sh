a67cba3479610ec5fa6f677aabdaf3de431209dc
yylemon9527 <32920982+yylemon9527@users.noreply.github.com>
2020-04-26 12:59:47 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,11 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
+wget "https://raw.githubusercontent.com/melsem/openwrt-lede_xradio-xr819_soc-audio/master/add-SOC_AUDIO-usb2-usb3-opi_zero_openwrt(lede)-18.06.xx/add-SOC_AUDIO-usb2-usb3-opi_zero_openwrt-18.06.xx.patch"
+wget "https://raw.githubusercontent.com/melsem/openwrt-lede_xradio-xr819_soc-audio/master/add-SOC_AUDIO-usb2-usb3-opi_zero_openwrt(lede)-18.06.xx/add-on_wifi-mac80211.patch"
+wget "https://raw.githubusercontent.com/melsem/openwrt-lede_xradio-xr819_soc-audio/master/packages_xradio-OpenWrt(Lede)-18.06.xx/add-patch_dts_file-wifi-xradio.patch"
+patch -p1 < add-SOC_AUDIO-usb2-usb3-opi_zero_openwrt-18.06.xx.patch
+patch -p1 < add-on_wifi-mac80211.patch
+patch -p1 < add-patch_dts_file-wifi-xradio.patch
